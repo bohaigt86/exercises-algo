980cd932e8d15e9fbf517c498aa5c281b040989e
Brian Liu <liubohai922@hotmail.com>
2019-07-11 12:10:56 +0800

    answers added

--- questions/reversestring/index.js
+++ questions/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const arr = str.split("");
+  return arr.reduce((rev, char) => char + rev, "");
+}
 
 module.exports = reverse;
+
+// Solution #1
+// function reverse(str) {
+//   const arr = str.split("");
+//   arr.reverse();
+//   return arr.join("");
+// }
+
+// Solution #2
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
