97f3f5811f4421fa2305e2317134ab5398679ffb
Brian Liu <liubohai922@hotmail.com>
2019-09-08 16:30:53 +0800

    new answers added

--- answers/capitalize/index.js
+++ answers/capitalize/index.js
@@ -7,6 +7,23 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // Solution #1
+  // const words = str
+  //   .split(' ')
+  //   .map(word => word[0].toUpperCase() + word.slice(1));
+  // return words.join(' ');
+
+  // Solution #2
+  let words = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      words += str[i].toUpperCase();
+    } else {
+      words += str[i];
+    }
+  }
+  return words;
+}
 
 module.exports = capitalize;
