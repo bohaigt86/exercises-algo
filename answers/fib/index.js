af1ceb9bf357b0290b34a8656042caedad5964dc
Brian Liu <liubohai922@hotmail.com>
2019-09-12 00:24:00 +0800

    apply memoization

--- answers/fib/index.js
+++ answers/fib/index.js
@@ -8,14 +8,30 @@
 // Example:
 //   fib(4) === 3
 
+// Use memoization, a concept frequently discussed in dynamic programming
+
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return 1;
+  // T(n) = O(2^n) not good!
+  if (n < 2) {
+    return n;
   }
   return fib(n - 1) + fib(n - 2);
 }
+
+// after using memoization, fib(15) will be as fast as fib(1), which only takes 1 ms
+fib = memoize(fib);
 
 module.exports = fib;
