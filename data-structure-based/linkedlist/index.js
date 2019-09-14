3410ab3c1d399cbf1c8070627221d0285ee9d756
Brian Liu <liubohai922@hotmail.com>
2019-09-14 14:57:32 +0800

    linkedlist updated

--- data-structure-based/linkedlist/index.js
+++ data-structure-based/linkedlist/index.js
@@ -64,6 +64,62 @@ class LinkedList {
     }
     prevNode.next = null;
   }
+  insertLast(data) {
+    const node = new Node(data);
+    const last = this.getLast();
+    if (last) {
+      last.next = node;
+    } else {
+      this.head = node;
+    }
+  }
+  getAt(index) {
+    if (!this.head) return null;
+
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) return;
+
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+
+    const prev = this.getAt(index - 1);
+    if (!prev || !prev.next) {
+      return;
+    }
+    prev.next = prev.next.next;
+  }
+
+  insertAt(data, index) {
+    if (!this.head) {
+      this.head = new Node(data);
+      return;
+    }
+
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const prev = this.getAt(index - 1) || this.getLast();
+
+    const node = new Node(data, prev.next);
+    prev.next = node;
+  }
 }
 
 module.exports = { Node, LinkedList };
