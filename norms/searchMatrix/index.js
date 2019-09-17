8e863f3c5f291bd875176ab269a083bf7af3e326
Brian Liu <liubohai922@hotmail.com>
2019-09-17 23:38:39 +0800

    new algorithm added

--- norms/searchMatrix/index.js
+++ norms/searchMatrix/index.js
@@ -4,35 +4,24 @@
  * - Integers in each column are sorted in ascending from top to bottom.
  */
 
-const searchMatrix = function(matrix, target) {
-  if (matrix.length === 0) return false;
-  const maxNums = matrix.map(array => array[array.length - 1]);
-  const minNums = matrix.map(array => array[0]);
-
-  if (maxNums.indexOf(target) > -1 || minNums.indexOf(target) > -1) {
-    return true;
-  } else if (maxNums[maxNums.length - 1] < target || minNums[0] > target) {
-    return false;
-  } else {
-    // include
-    const startRow = maxNums.indexOf(maxNums.find(maxNum => maxNum > target));
-    console.log(startRow);
-    // exclude
-    const endRow =
-      minNums.indexOf(minNums.find(minNum => minNum > target)) !== -1
-        ? minNums.indexOf(minNums.find(minNum => minNum > target))
-        : minNums.length;
-    console.log(endRow);
-
-    for (var i = startRow; i < endRow; i++) {
-      for (var j = 0; j < matrix[i].length; j++) {
-        if (matrix[i][j] === target) {
-          return true;
-        }
-      }
-    }
+var searchMatrix = function(matrix, target) {
+  if (matrix.length < 1 || matrix[0].length < 1) {
     return false;
   }
+
+  let row = matrix.length - 1;
+  let col = 0;
+
+  while (row >= 0 && col < matrix[0].length) {
+    if (matrix[row][col] === target) {
+      return true;
+    } else if (matrix[row][col] > target) {
+      row--;
+    } else {
+      col++;
+    }
+  }
+  return false;
 };
 
 module.exports = searchMatrix;
