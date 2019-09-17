c0be52ef87827efd1aa62e9b1a9c1902dee7307d
Brian Liu <liubohai922@hotmail.com>
2019-09-17 20:52:08 +0800

    folder name changed

--- norms/2dArraySearch/test.js
+++
@@ -1,5 +0,0 @@
-const searchMatrix = require('./index.js');
-
-test('searchMatrix function exists', () => {
-  expect(typeof searchMatrix).toEqual('function');
-});
---
+++ norms/searchMatrix/test.js
@@ -0,0 +1,11 @@
+const searchMatrix = require('./index.js');
+
+test('searchMatrix function exists', () => {
+  expect(typeof searchMatrix).toEqual('function');
+});
+
+// test('maxInt returns a largest possible int in string', () => {
+//   expect(maxInt([10, 2])).toEqual('210');
+// });
+
+test('searchMatrix', () => {});
