8e57fe28a4d50223094954b581b973f53f01670a
Brian Liu <liubohai922@hotmail.com>
2019-09-24 22:58:14 +0800

    new content added

--- data-structure/5-1-bst-validate/index.js
+++ data-structure/5-1-bst-validate/index.js
@@ -5,6 +5,33 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (max && node.data > max) {
+    return false;
+  }
+
+  if (min && node.data < min) {
+    return false;
+  }
+  // One way of doing it
+  // if (node.left && !validate(node.left, min, node.data)) {
+  //   return false;
+  // }
+
+  // if (node.right && !validate(node.right, node.data, max)) {
+  //   return false;
+  // }
+
+  // Another way of doing it
+  if (node.left) {
+    return validate(node.left, min, node.data);
+  }
+
+  if (node.right) {
+    return validate(node.right, node.data, max);
+  }
+
+  return true;
+}
 
 module.exports = validate;
