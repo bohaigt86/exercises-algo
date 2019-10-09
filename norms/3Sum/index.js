6d0049b4e873c8df08f36054d63b402a9ced09b7
Brian Liu <liubohai922@hotmail.com>
2019-10-10 03:25:56 +0800

    new content added

--- norms/3Sum/index.js
+++ norms/3Sum/index.js
@@ -1,3 +1,40 @@
-function threeSum(array) {}
+function threeSum(arr) {
+  let results = [];
+  if (arr.length > 2) {
+    arr.sort((a, b) => a - b);
+    const len = arr.length;
+
+    if (arr[0] <= 0 && arr[len - 1] >= 0) {
+      for (let i = 0; i < len - 1; i++) {
+        if (arr[i] > 0) break;
+        if (arr[i] === arr[i - 1]) continue;
+
+        let result,
+          first = i + 1,
+          last = len - 1;
+
+        while (first < last) {
+          if (arr[first] === arr[first - 1] && first > i + 1) {
+            first++;
+          } else if (arr[last] === arr[last + 1] && last < len - 1) {
+            last--;
+          } else {
+            result = arr[i] + arr[first] + arr[last];
+            if (result === 0) {
+              results.push([arr[i], arr[first], arr[last]]);
+              first++;
+              last--;
+            } else if (result > 0) {
+              last--;
+            } else {
+              first++;
+            }
+          }
+        }
+      }
+    }
+  }
+  return results;
+}
 
 module.exports = threeSum;
--- norms/3Sum/test.js
+++ norms/3Sum/test.js
@@ -6,6 +6,24 @@ test('function threeSum is defined', () => {
 
 test('function threeSum returns [[-1,0,1],[-1,-1,2]], given [-1, 0, 1, 2, -1, -4]', () => {
   const input = [-1, 0, 1, 2, -1, -4];
-  const result = [[-1, 0, 1], [-1, -1, 2]];
+  const result = [[-1, -1, 2], [-1, 0, 1]];
+  expect(threeSum(input)).toEqual(result);
+});
+
+test('function threeSum returns [[0,0,0]], given [0, 0, 0]', () => {
+  const input = [0, 0, 0];
+  const result = [[0, 0, 0]];
+  expect(threeSum(input)).toEqual(result);
+});
+
+test('function threeSum returns [[0, 0, 0]], given [0, 0, 0, 0]', () => {
+  const input = [0, 0, 0, 0];
+  const result = [[0, 0, 0]];
+  expect(threeSum(input)).toEqual(result);
+});
+
+test('function threeSum returns [[-1, 0, 1]], given [-1, 0, 1, 0]', () => {
+  const input = [-1, 0, 1, 0];
+  const result = [[-1, 0, 1]];
   expect(threeSum(input)).toEqual(result);
 });
